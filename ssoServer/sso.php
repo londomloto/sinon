b338a1c2cc4505bd00a5154b7d22c1eff3d90d1a
almibiakmal <ardiprayuda11@gmail.com>
2017-01-28 23:34:00 +0700

    Versi 1.0

--- ssoServer/sso.php
+++ ssoServer/sso.php
@@ -2,8 +2,15 @@
 
 abstract class SSO {
 
-    function __construct() {
-        
+    protected $options = ['files_cache_directory' => '/tmp', 'files_cache_ttl' => 36000];
+    protected $cache;
+    protected $returnType;
+    protected $appsId;
+    
+    public function __construct(array $options = [])
+    {
+        $this->options = $options + $this->options;
+        $this->cache = $this->createCacheAdapter();
     }
 
 }
